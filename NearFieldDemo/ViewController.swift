0ad444fc9c5ef58adc15079bcc72a98984e2cfc0
Simon Heij <sheij1@avans.nl>
2019-02-08 10:00:01 +0100

    Initial Commit

---
+++ NearFieldDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearFieldDemo
+//
+//  Created by simon heij on 08-02-19.
+//  Copyright © 2019 simon heij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
